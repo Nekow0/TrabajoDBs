17d3f0605f69167995ed4cbb1ccf99dbc68bac5a
Javiera Ayala Navarro <jayalnav@emeal.nttdata.com>
2021-11-26 09:55:04 -0300

    Proyecto Base

---
+++ src/main/java/com/tiendaVirtual/ActividadGrupalApplication.java
@@ -0,0 +1,13 @@
+package com.tiendaVirtual;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ActividadGrupalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ActividadGrupalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tiendaVirtual/ActividadGrupalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tiendaVirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ActividadGrupalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
